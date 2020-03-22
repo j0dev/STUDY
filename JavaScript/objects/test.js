cb4281f1131acc39f562ebd240ba99d124e7c579
j0dev <j0dev>
2020-03-22 21:46:43 +0900

    object

---
+++ JavaScript/objects/test.js
@@ -0,0 +1,15 @@
+const dogName = '강아지'
+const dogAge = 2;
+
+const dog = {
+	name : '강아지',
+	dogAge : 2,
+	cute : true,
+	'space test': true,
+	sample: {
+		a: 1,
+		b: 2,
+	}
+}
+
+console.log(dog.name)
