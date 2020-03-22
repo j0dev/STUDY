21daad2eafd076cadaecf7ac6e9666c5bcd77e70
j0dev <j0dev>
2020-03-22 21:45:56 +0900

    function

---
+++ JavaScript/function/test.js
@@ -0,0 +1,22 @@
+function test() {
+	
+	return 0
+}
+//General Function Expression
+
+
+
+
+const hello1 = (name) => name
+
+const hello2 = (name) => {
+	return name
+}
+
+const add1 = (a, b) => {
+	return a+b
+}
+
+const add2 = (a, b) => a+b
+
+//Arrow Function Expression
