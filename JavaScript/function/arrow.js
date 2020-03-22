210ffe543157f15bfeab16214ba66a207a1b6649
j0dev <j0dev>
2020-03-22 21:52:16 +0900

    Arrow function Expression

---
+++ JavaScript/function/arrow.js
@@ -0,0 +1,13 @@
+const hello1 = (name) => name
+
+const hello2 = (name) => {
+        return name
+}
+
+const add1 = (a, b) => {
+        return a+b
+}
+
+const add2 = (a, b) => a+b
+
+//Arrow Function Expression
